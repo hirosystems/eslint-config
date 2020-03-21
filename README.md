b65c111ec965dc973f6e3438f128cb6bf2924a24
Hank Stoever <hstove@gmail.com>
2020-03-21 08:56:21 -0700

    feat: first set of ESLint config

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# .github
-Repository to hold defaults such as CONTRIBUTING and CODE_OF_CONDUCT
+# @blockstack/prettier-config
+
+A set of ESLint configuration and rules for use in Blockstack internal projects.
+
+## Installation
+
+~~~bash
+yarn add --dev eslint @blockstack/eslint-config
+# or, with npm
+npm install --save-dev eslint @blockstack/eslint-config
+~~~
+
+Then, create or modify your `.eslintrc.js` file to extend this config:
+
+~~~js
+module.exports = {
+  extends: "@blockstack"
+}
+~~~
+
+Finally, modify your `package.json` file to use our prettier config, which is already installed as a dependency of this package.
+
+~~~json
+{
+  "prettier": "@blockstack/prettier-config"
+}
+~~~
+
+## Overriding rules
+
+This configuration includes a bunch of rules that have become standard in our JavaScript projects. However, if you feel the need to override a rule, you can always do so by simply adding `rules` to your `.eslintrc.js` file.
+
+~~~js
+module.exports = {
+  extends: ["@blockstack"],
+  rules: {
+    "@typescript-eslint/no-use-before-define": [2],
+  }
+}
+~~~
---
+++ index.js
@@ -0,0 +1,80 @@
+module.exports = {
+  "extends": [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "project": "./tsconfig.json"
+  },
+  "plugins": [
+    "import",
+    "@typescript-eslint"
+  ],
+  "settings": {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts"]
+    },
+    "import/resolver": { "typescript": {} }
+  },
+  "env": {
+    "browser": true,
+    "node": true
+  },
+  "rules": {
+    "no-tabs": 0,
+    "no-restricted-globals": 0,
+    "import/prefer-default-export": 0,
+    "no-buffer-constructor": 0,
+    "no-mixed-operators": 0,
+    "no-plusplus": 0,
+    "no-bitwise": 0,
+    "prefer-promise-reject-errors": 0,
+    "class-methods-use-this": 0,
+    "import/no-cycle": 0,
+    "prefer-destructuring": 0,
+    "no-prototype-builtins": 0,
+    "eol-last": 2,
+    "no-debugger": 1,
+    "no-mixed-requires": 0,
+    "no-underscore-dangle": 0,
+    "no-multi-spaces": 0,
+    "no-trailing-spaces": 0,
+    "no-extra-boolean-cast": 0,
+    "no-var": 2,
+    "no-param-reassign": 0,
+    "no-else-return": 0,
+    "no-console": 0,
+    "prefer-const": 2,
+    "new-cap": 0,
+    "semi": 0,
+    "valid-jsdoc": "off",
+    "object-curly-newline": "off",    
+    "arrow-parens": "off",
+    "function-paren-newline": 0,
+    "no-shadow": "off",
+    "no-use-before-define": "off",
+    "@typescript-eslint/explicit-member-accessibility": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/class-name-casing": "off",
+    "@typescript-eslint/camelcase": "off",
+    "@typescript-eslint/array-type": "off",
+    "@typescript-eslint/member-delimiter-style": "off",
+    "@typescript-eslint/no-angle-bracket-type-assertion": "off",
+    "@typescript-eslint/prefer-interface": "off",
+    "@typescript-eslint/no-use-before-define": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+
+    // TODO: enable this when reasonable
+    "@typescript-eslint/no-explicit-any": "off",
+
+    // TODO: enable this when reasonable
+    "@typescript-eslint/promise-function-async": "off",
+
+    // TODO: enable when this is fixed https://github.com/benmosher/eslint-plugin-import/issues/1282
+    "import/named": "off"
+  }
+}
